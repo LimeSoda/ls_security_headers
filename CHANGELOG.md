033f7c5db3583533677d6034f7f20a05a0e3ea97
Rudolf Rohaczek <r.rohaczek@limesoda.com>
2024-10-22 12:03:25 +0200

    Prepare release 2.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [2.0.0] - 22-10-2024
+
 ### Added
 - Add support for nonce attribute output in `generateNonce` method
 - Add site set
--- ext_emconf.php
+++ ext_emconf.php
@@ -9,11 +9,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'LimeSoda Interactive Marketing GmbH',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '1.3.1',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.9.99',
-            'php' => '7.4.0-8.2.99',
+            'typo3' => '13.4.0-13.4.99',
+            'php' => '8.2.0-8.3.99',
         ],
         'conflicts' => [],
         'suggests' => [],
